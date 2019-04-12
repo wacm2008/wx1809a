90df6336732c6a443fb94f671e6c99b5c059d9c2
wacm2008 <gl.star@163.com>
2019-04-12 08:01:51 +0000

    新微信2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hola
                 </div>
 
                 <div class="links">
